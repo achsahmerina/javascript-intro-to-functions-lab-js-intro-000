81fe1156ba0eb1d326795ad27501ed0010ffbd45
Achsah Abraham <achsahmp@gmail.com>
2018-02-14 23:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log('hello')
 }
 function sayHiToGrandma(string){
-  if ("YES INDEED!"){
+  if (string === "YES INDEED!"){
     return ("I can\'t hear you!")
   }
 }
