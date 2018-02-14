8952ccd3f088afd21f6bef318f5350123eee6c4d
Achsah Abraham <achsahmp@gmail.com>
2018-02-14 22:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log( 'HELLO' )
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  return("I can't hear you!"){
+    if(string === .toLowerCase)
+  }
 }
